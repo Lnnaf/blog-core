182968a912f558301385605ff9d54d0a9ec99b16
linh nguyen van <linh.nguyenvan@axonactive.com>
2023-05-12 17:33:45 +0700

    init project

---
+++ src/test/java/lyn/blog/core/BlogCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package lyn.blog.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
